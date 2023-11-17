33f17a10dd1801c299982398a0e4d2254c544cfa
Owen Stelmarski <hypedsupremelife@gmail.com>
2023-11-16 20:11:42 -0600

    add theme to renderOptions

--- src/makes/Canny.ts
+++ src/makes/Canny.ts
@@ -1,47 +1,46 @@
 export type RenderOptions = {
-    boardToken: string;
-    basePath?: string;
-    user?: {
-        id?: any;
-        email?: any;
-    };
-    ssoToken?: string;
-    onLoadCallback?: () => void;
+  boardToken: string;
+  basePath?: string;
+  user?: {
+    id?: any;
+    email?: any;
+  };
+  ssoToken?: string;
+  onLoadCallback?: () => void;
+  theme?: "dark" | "light" | "auto";
 };
 
 export type ChangeLogOptions = {
-    appID: string;
-    align: "top" | "bottom" | "left" | "right";
-    position: "top" | "bottom" | "left" | "right";
-    labelIDs?: string[];
+  appID: string;
+  align: "top" | "bottom" | "left" | "right";
+  position: "top" | "bottom" | "left" | "right";
+  labelIDs?: string[];
 };
 
-
 class Canny {
-    canny: any;
+  canny: any;
 
-    constructor(canny: any) {
-        this.canny = canny;
-    }
+  constructor(canny: any) {
+    this.canny = canny;
+  }
 
-    render(options: RenderOptions) {
-        if(this.canny) {
-            this.canny("render", options);
-        }
+  render(options: RenderOptions) {
+    if (this.canny) {
+      this.canny("render", options);
     }
+  }
 
-    initChangelog(options: ChangeLogOptions) {
-        if(this.canny) {
-            this.canny("initChangelog", options);
-        }
+  initChangelog(options: ChangeLogOptions) {
+    if (this.canny) {
+      this.canny("initChangelog", options);
     }
+  }
 
-    closeChangelog() {
-        if(this.canny) {
-            this.canny("closeChangelog");
-        }
+  closeChangelog() {
+    if (this.canny) {
+      this.canny("closeChangelog");
     }
+  }
 }
 
-
-export {Canny};
+export { Canny };
