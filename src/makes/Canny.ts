ee39bb3d5538b3694c7688f12f84b86cb03b9b5e
Owen Stelmarski <hypedsupremelife@gmail.com>
2023-11-20 17:41:46 -0600

    comment updates
    - change indentation to 4
    - change package version to 0.0.5
    - add pnpm-lock.yaml to gitignore/npmignore

--- src/makes/Canny.ts
+++ src/makes/Canny.ts
@@ -1,46 +1,46 @@
 export type RenderOptions = {
-  boardToken: string;
-  basePath?: string;
-  user?: {
-    id?: any;
-    email?: any;
-  };
-  ssoToken?: string;
-  onLoadCallback?: () => void;
-  theme?: "dark" | "light" | "auto";
-};
-
-export type ChangeLogOptions = {
-  appID: string;
-  align: "top" | "bottom" | "left" | "right";
-  position: "top" | "bottom" | "left" | "right";
-  labelIDs?: string[];
-};
-
-class Canny {
-  canny: any;
-
-  constructor(canny: any) {
-    this.canny = canny;
-  }
-
-  render(options: RenderOptions) {
-    if (this.canny) {
-      this.canny("render", options);
+    boardToken: string
+    basePath?: string
+    user?: {
+        id?: any
+        email?: any
     }
-  }
-
-  initChangelog(options: ChangeLogOptions) {
-    if (this.canny) {
-      this.canny("initChangelog", options);
-    }
-  }
-
-  closeChangelog() {
-    if (this.canny) {
-      this.canny("closeChangelog");
-    }
-  }
+    ssoToken?: string
+    onLoadCallback?: () => void
+    theme?: 'dark' | 'light' | 'auto'
 }
 
-export { Canny };
+export type ChangeLogOptions = {
+    appID: string
+    align: 'top' | 'bottom' | 'left' | 'right'
+    position: 'top' | 'bottom' | 'left' | 'right'
+    labelIDs?: string[]
+}
+
+class Canny {
+    canny: any
+
+    constructor(canny: any) {
+        this.canny = canny
+    }
+
+    render(options: RenderOptions) {
+        if (this.canny) {
+            this.canny('render', options)
+        }
+    }
+
+    initChangelog(options: ChangeLogOptions) {
+        if (this.canny) {
+            this.canny('initChangelog', options)
+        }
+    }
+
+    closeChangelog() {
+        if (this.canny) {
+            this.canny('closeChangelog')
+        }
+    }
+}
+
+export { Canny }
--- src/views/CannyFeedback/index.tsx
+++ src/views/CannyFeedback/index.tsx
@@ -1,43 +1,43 @@
-import React, { useRef, useEffect, ElementType } from "react";
+import React, { useRef, useEffect, ElementType } from 'react'
 
-import { useCannyContext } from "../../contexts";
-import { RenderOptions } from "../../makes";
+import { useCannyContext } from '../../contexts'
+import { RenderOptions } from '../../makes'
 
 type Props = RenderOptions & {
-  component?: ElementType;
-};
+    component?: ElementType
+}
 
 const CannyFeedback: React.FC<Props> = (props: Props) => {
-  const {
-    component: Component = "div",
-    basePath,
-    boardToken,
-    ssoToken,
-    theme,
-    onLoadCallback,
-    ...rest
-  } = props;
-
-  const handleLoad = useRef<typeof onLoadCallback>();
-
-  handleLoad.current = onLoadCallback;
-
-  const { isLoaded, canny } = useCannyContext();
-
-  useEffect(() => {
-    if (isLoaded) {
-      canny.render({
+    const {
+        component: Component = 'div',
         basePath,
         boardToken,
         ssoToken,
         theme,
-        onLoadCallback: handleLoad.current,
-      });
-    }
-  }, [ssoToken, isLoaded, basePath, boardToken]);
+        onLoadCallback,
+        ...rest
+    } = props
 
-  return <Component {...rest} data-canny="" />;
-};
+    const handleLoad = useRef<typeof onLoadCallback>()
 
-export type { Props as CannyFeedbackProps };
-export { CannyFeedback };
+    handleLoad.current = onLoadCallback
+
+    const { isLoaded, canny } = useCannyContext()
+
+    useEffect(() => {
+        if (isLoaded) {
+            canny.render({
+                basePath,
+                boardToken,
+                ssoToken,
+                theme,
+                onLoadCallback: handleLoad.current,
+            })
+        }
+    }, [ssoToken, isLoaded, basePath, boardToken])
+
+    return <Component {...rest} data-canny="" />
+}
+
+export type { Props as CannyFeedbackProps }
+export { CannyFeedback }
